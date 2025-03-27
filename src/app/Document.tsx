68186d229c1af8ebdde78dd27c617826555ca970
Herman Stander <her.stander@gmail.com>
2025-03-27 12:52:26 +0200

    updated favicon

--- src/app/Document.tsx
+++ src/app/Document.tsx
@@ -38,7 +38,7 @@ export const Document: React.FC<{ children: React.ReactNode; nonce?: string }> =
       <meta name="sitemap" content="/sitemap.xml" />
       <meta httpEquiv="Content-Security-Policy" content="script-src 'self' 'unsafe-inline' https://challenges.cloudflare.com https://www.google-analytics.com https://www.googletagmanager.com https://buttons.github.io https://kwesforms.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; connect-src 'self' https://api.github.com https://kwesforms.com https://kwesforms.com/api/foreign/forms/* https://www.google-analytics.com; frame-src https://tagmanager.google.com; object-src 'none';" />
       <title>RedwoodSDK</title>
-      <link rel="icon" type="image/svg+xml" href="/images/favicon.ico" />
+      <link rel="icon" type="image/svg+xml" href="/images/favicon.svg" />
       <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} nonce={nonce}></script>
       <script dangerouslySetInnerHTML={{ __html: gaScript }} nonce={nonce} />
       <script type="module" src="/src/client.tsx" nonce={nonce}></script>
