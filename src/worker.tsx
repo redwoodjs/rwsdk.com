df7bd88ed8911ba0b810f34b3363c40a242a36d3
Herman Stander <her.stander@gmail.com>
2025-03-31 13:47:14 +0200

    addeding readme page

--- src/worker.tsx
+++ src/worker.tsx
@@ -4,6 +4,7 @@ import { Document } from "src/Document";
 import Home from "src/pages/Home";
 import { setCommonHeaders } from "src/headers";
 import sitemap from "./sitemap";
+import ReadMe from "src/pages/readme/ReadMe";
 
 type Context = {};
 
@@ -15,6 +16,7 @@ export default defineApp<Context>([
   },
   document(Document, [
     index([Home]),
+    route("/readme", [ReadMe]),
     route("/docs", async () => {
       return new Response(null, {
         status: 301,
