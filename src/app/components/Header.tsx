e7a6f27c16121cc4e2e3f68121f7da4b19e45bb5
Herman Stander <her.stander@gmail.com>
2025-04-02 11:02:24 +0200

    make titles and font smaller

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -8,8 +8,8 @@ export default function Header() {
   return (
     <div className="z-10 py-4 px-6 md:pr-16 flex items-center flex-col md:block">
       <div className="flex justify-end">
-        <span className="font-jersey-charted text-orange font-light tracking-tight leading-none text-[20px] md:mb-10 mb-2 mx-2">
-          PS | {formattedDate}
+        <span className="font-light tracking-tight leading-none text-[20px] md:mb-10 mb-2 mx-2">
+          PERSONAL SOFTWARE | <span className="text-orange">{formattedDate}</span>
         </span>
       </div>
       <a href="/" aria-label="RedwoodSDK">
--- src/app/components/HeroImage.tsx
+++ src/app/components/HeroImage.tsx
@@ -1,7 +1,7 @@
 export default function HeroImage() {
   return (
     <div className="md:w-1/3 hidden md:block">
-      <div className="sticky top-18 h-screen w-full flex items-center justify-center">
+      <div className="sticky top-0 h-screen w-full flex items-center justify-center">
         <img src="/images/hero.png" alt="Hero Image" className="w-full h-full object-cover" />
       </div>
     </div>
