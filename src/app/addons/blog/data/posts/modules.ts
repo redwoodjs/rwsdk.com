026bedd27b5b6d893bf3bcef87b744c60f2e23fa
Martijn van der Ven <martijn@vanderven.se>
2026-04-05 23:11:25 +0200

    Fix compatibility issues from dependency upgrades

--- src/app/addons/blog/data/posts/modules.ts
+++ src/app/addons/blog/data/posts/modules.ts
@@ -1,5 +1,5 @@
 // Use Vite's import.meta.glob to get all markdown files
-const blogModules = import.meta.glob('./*.md', { as: 'raw', eager: true });
+const blogModules = import.meta.glob('./*.md', { query: '?raw', import: 'default', eager: true });
 
 // Get all available blog post slugs
 const blogPostSlugs = Object.keys(blogModules).map(key => key.replace('./', '').replace('.md', ''));
